ab980823a76affebe4f20486ca334ee478ae79fe
Stephen Ho <wickedcz4a@gmail.com>
2022-10-01 16:24:59 -0700

    implemented swipe and 2 buttons

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home/home.js';
+import Swipe from './components/Home/swipe.js';
+import CardSwipe from './components/Home/cardSwipe.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <CardSwipe/>
       <StatusBar style="auto" />
     </View>
   );
