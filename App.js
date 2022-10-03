511075e997578694f7750d58c71ae462a1e9741b
Stephen Ho <wickedcz4a@gmail.com>
2022-10-03 10:11:54 -0700

    implemented navbar

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CardSwipe from './components/Home/cardSwipe.js';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Entypo, FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  function renderHome () {
+    return <Entypo name="home" size={24} color="black" />;
+  }
+
+  function renderMap () {
+    return <FontAwesome name="map-marker" size={24} color="black" />;
+  }
+
+  function renderEvents () {
+    return <MaterialIcons name="event-note" size={24} color="black" />;
+  }
+
+  function renderMatches () {
+    return <MaterialCommunityIcons name="heart-multiple" size={24} color="black" />;
+  }
+
+  function renderProfile () {
+    return <MaterialIcons name="account-box" size={24} color="black" />;
+  }
+
   return (
-    <View style={styles.container}>
-      <CardSwipe/>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <CardSwipe/>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Map"
+          component={CardSwipe}
+          options={{ headerShown: false, tabBarIcon: renderMap, tabBarActiveTintColor: 'black', tabBarActiveBackgroundColor: '#CDB4DB' }}
+        />
+        <Tab.Screen
+          name="Events"
+          component={CardSwipe}
+          options={{ headerShown: false, tabBarIcon: renderEvents, tabBarActiveTintColor: 'black', tabBarActiveBackgroundColor: '#CDB4DB' }}
+        />
+        <Tab.Screen
+          name="Home"
+          component={CardSwipe}
+          options={{ headerShown: false, tabBarIcon: renderHome, tabBarActiveTintColor: 'black', tabBarActiveBackgroundColor: '#CDB4DB' }}
+        />
+        <Tab.Screen
+          name="Matches"
+          component={CardSwipe}
+          options={{ headerShown: false, tabBarIcon: renderMatches, tabBarActiveTintColor: 'black', tabBarActiveBackgroundColor: '#CDB4DB' }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={CardSwipe}
+          options={{ headerShown: false, tabBarIcon: renderProfile, tabBarActiveTintColor: 'black', tabBarActiveBackgroundColor: '#CDB4DB' }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
