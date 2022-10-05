87a47f3b4f42ea0e4cf49a717e7b2b545e231b64
Daniel Au <daniel.au05@gmail.com>
2022-10-05 12:28:10 -0700

    Added confirm match functionality

--- components/Requests/requests.js
+++ components/Requests/requests.js
@@ -5,6 +5,7 @@ import { ListItem, Avatar, Text, Tab, TabView} from "@rneui/themed";
 import styles from './requestStyles.js';
 import Chat from '../../chat/Chat.js';
 import { AntDesign, Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
 
 const users = [
   {
@@ -48,9 +49,27 @@ const matched = [
 const Request = (props) => {
   const [index, setIndex] = useState(0);
   const [isChat, setIsChat] = useState(false);
+  const [pending, setPending] = useState([]);
+  const [accepted, setAccepted] = useState([]);
 
   const toggleChat = () => {
     setIsChat(!isChat);
+  }
+
+  useEffect(() => {
+    axios.get('http://54.219.129.63:3000/matches/Justin/Max/pending')
+      .then((response) => { setPending(response.data) })
+      .catch((err) => {console.log('Error getting pending matches')})
+
+    axios.get('http://54.219.129.63:3000/matches/Justin/Max/confirmed')
+    .then((response) => { console.log(response.data); setAccepted(response.data)} )
+    .catch((err) => {console.log('Error getting confirmed matches')})
+  }, [])
+
+  const confirmMatch = () => {
+    const match = {"dog1_name": "Max", "owner1_name": "Justin", "dog2_name": "Coco", "owner2_name": "Alex"}
+    axios.post('http://54.219.129.63:3000/matches', match)
+      .catch((err) => {console.log('Error confirming match')})
   }
 
   return (
@@ -81,19 +100,19 @@ const Request = (props) => {
       <TabView value={index} onChange={setIndex} animationType='spring'>
         <TabView.Item style={styles.tabView}>
           <FlatList
-            data={users}
+            data={pending}
             renderItem={({item}) => (
               <ListItem bottomDivider>
-                <Avatar rounded source={{uri: item.avatar}} size={60} />
+                <Avatar rounded source={{uri: item.photos[0]}} size={60} />
                 <ListItem.Content>
-                  <ListItem.Title style={styles.name}>{item.name}</ListItem.Title>
+                  <ListItem.Title style={styles.name}>{item.dog_name}</ListItem.Title>
                   <ListItem.Subtitle>{item.breed}</ListItem.Subtitle>
                   <ListItem.Subtitle>{item.personality}</ListItem.Subtitle>
                   <ListItem.Subtitle>{item.size}</ListItem.Subtitle>
                 </ListItem.Content>
                 <View style={styles.buttons}>
                   <AntDesign name="closecircle" size={24} color="#FF5733" onPress={() => {alert('Declined')}}/>
-                  <AntDesign name="checkcircle" size={24} color="#0BDA51" style={styles.closecircle} onPress={() => {alert('Accepted')}}/>
+                  <AntDesign name="checkcircle" size={24} color="#0BDA51" style={styles.closecircle} onPress={confirmMatch}/>
                 </View>
               </ListItem>
             )}
