3c9ffaa9838c59ed27c9d95262f68073bdf87fc9
mfitz6022 <mfitz6022@gmail.com>
2022-10-07 10:57:22 -0700

    RIP last message :(

--- components/Requests/requests.js
+++ components/Requests/requests.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { View, FlatList, SafeAreaView } from 'react-native';
 import { ListItem, Avatar, Text, Tab, TabView} from "@rneui/themed";
+import { auth, db } from '../../firebase.js';
 import styles from './requestStyles.js';
 import Chat from '../../chat/Chat.js';
 import { AntDesign, Ionicons } from '@expo/vector-icons';
@@ -15,6 +16,7 @@ const Request = (props) => {
   const [accepted, setAccepted] = useState([]);
   const [currentMatch, setCurrentMatch] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [lastMessage, setLastMessage] = useState('hardcode');
   const owner_name = props.route.params.user;
   const dogName = props.route.params.dog.dog_name;
   const userPhoto = props.route.params.dog.photos[0];
@@ -105,6 +107,21 @@ const Request = (props) => {
       .catch((err) => {console.log('Error confirming match')})
   }
 
+  // const fetchLastMessage = (matchId) => {
+  //   //let finalMessage = 'No Messages yet!';
+  //   return db.collection(`room: ${matchId}`)
+  //   .orderBy('createdAt', 'desc')
+  //   .limit(1)
+  //   .onSnapshot((snapshot) => {
+  //     if (snapshot.docs[0])
+  //       return snapshot.docs[0].data().text;
+  //     else {
+  //       return 'No Messages yet!'
+  //     }
+  //   })
+  //   // return finalMessage;
+  // }
+
   if (!isLoading) {
     return (
       <SafeAreaView style={{flex: 1, backgroundColor: '#d9edff'}}>
@@ -169,9 +186,10 @@ const Request = (props) => {
                   }}>
                     <Avatar rounded source={{uri: item.dog2_photos[0]}} size={60} />
                     <ListItem.Content>
-                      <ListItem.Title style={styles.name}>{item.dog2_dog}</ListItem.Title>
-                      <ListItem.Subtitle>Test message
-                      </ListItem.Subtitle>
+                      <View style={styles.acceptedProfiles}>
+                        <ListItem.Title style={styles.name}>{item.dog2_dog}</ListItem.Title>
+                        <Ionicons name="chatbox-ellipses-outline" size={24} color="black" />
+                      </View>
                     </ListItem.Content>
                   </ListItem>
                 )}
